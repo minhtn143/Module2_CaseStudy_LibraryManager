bf3ee785985090d75cc28a543f3eeee99abeb101
minhtn143 <minhtn143@gmail.com>
2020-05-13 16:53:13 +0700

    done BookDB.php.php

--- library/controller/BookController.php
+++ library/controller/BookController.php
@@ -4,7 +4,21 @@
 namespace controller;
 
 
+use model\BookDB;
+use model\DBConnect;
+
 class BookController
 {
+    protected $bookDB;
 
+    public function __construct()
+    {
+        $db = new DBConnect();
+        $this->bookDB = new BookDB($db->connect());
+    }
+
+    public function add()
+    {
+
+    }
 }
--- library/model/book/BookDB.php
+++ library/model/book/BookDB.php
@@ -1,7 +1,26 @@
 <?php
-
+namespace model;
 
 class BookDB
 {
+    protected $conn;
 
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function add($book)
+    {
+        $sql = "INSERT INTO tblbook('booktitle','bookauthors','subjectid','mdescription','publisher', 'copyrightyear') 
+                VALUES (?,?,?,?,?,?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(1, $book->getTitle());
+        $stmt->bindParam(2, $book->getAuthors());
+        $stmt->bindParam(3, $book->getSubjectId());
+        $stmt->bindParam(4, $book->getDescription());
+        $stmt->bindParam(5, $book->getPublisher());
+        $stmt->bindParam(6, $book->getCopyrightYear());
+        $stmt->exexcute();
+    }
 }
